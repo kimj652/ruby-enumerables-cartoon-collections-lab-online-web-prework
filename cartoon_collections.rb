6e89054204b93bfbc5cd1b41575455bd3671772f
J K <kim.ju652@gmail.com>
2019-07-23 23:50:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)# code an argument here
     |x,i| puts "#{i + 1}. #{x}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
 end
 
