516f28376ce32b460c8754d160ab1f8a63fd1cbb
J K <kim.ju652@gmail.com>
2019-07-23 23:52:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array
+  array.collect { 
+    |x| x
+  }
 end
 
 def long_planeteer_calls# code an argument here
