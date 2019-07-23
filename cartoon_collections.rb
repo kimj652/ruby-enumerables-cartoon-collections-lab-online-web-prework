ff823f3b37a2a30fbce16ca791373747ed92b89a
J K <kim.ju652@gmail.com>
2019-07-23 23:47:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index {
     |i,x| puts "#{x[i]}" }
-  end
 end
 
 def summon_captain_planet# code an argument here
